8ea75f43f9fc73fba4a35bd2b80b74c7df410820
Jori Lampi <36279434+jolampi@users.noreply.github.com>
2025-05-30 14:48:25 +0300

    Simple login page

---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,46 @@
+import { authenticate, deauthenticate, isAuthenticated } from "@/services/backend";
+import React, { useEffect, useState } from "react";
+
+const LoginPage: React.FC = () => {
+  const [authenticated, setAuthenticated] = useState(false)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    setAuthenticated(isAuthenticated())
+  }, [])
+
+  const handleLogin = async () => {
+    const result = await authenticate({ username, password })
+    setAuthenticated(result)
+    setUsername("")
+    setPassword("")
+  }
+
+  const handleLogout = async () => {
+    deauthenticate()
+    setAuthenticated(false)
+  }
+
+  return <div>
+    <div>
+      Logged in: {authenticated ? 'yes' : 'no'}
+      {authenticated && <button onClick={handleLogout}>Logout</button>}
+    </div>
+    <div>
+      <label>
+        Username
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </label>
+    </div>
+    <div>
+      <label>
+        Password
+        <input type="password" value={password}  onChange={(e) => setPassword(e.target.value)} />
+      </label>
+    </div>
+    <button onClick={handleLogin}>Login</button>
+  </div>
+}
+
+export default LoginPage
---
+++ frontend/src/services/backend.ts
@@ -0,0 +1,29 @@
+import { postApiAuthLogin } from "@/generated/client";
+import Cookies from "universal-cookie";
+
+const ACCESS_TOKEN_COOKIE = "access_token";
+
+const cookies = new Cookies();
+
+export interface Credentials {
+  username: string
+  password: string
+}
+
+export async function authenticate(credentials: Credentials): Promise<boolean> {
+  const response = await postApiAuthLogin({ body: credentials });
+  if (!response.data) {
+    return false;
+  }
+  // TODO: Address security concerns properly
+  cookies.set(ACCESS_TOKEN_COOKIE, response.data.accessToken)
+  return true;
+}
+
+export function isAuthenticated() {
+  return !!cookies.get(ACCESS_TOKEN_COOKIE)
+}
+
+export function deauthenticate() {
+  cookies.remove(ACCESS_TOKEN_COOKIE)
+}
