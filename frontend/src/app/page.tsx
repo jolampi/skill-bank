f4860d1eacdccc0d4ae8537c94124d69f57ee8c1
Jori Lampi <36279434+jolampi@users.noreply.github.com>
2025-06-04 16:14:57 +0300

    Add navigation bar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,25 +1,14 @@
 "use client";
 
-import { useContext } from "react";
-import AuthContext from "@/contexts/AuthContext";
 import withAuthentication from "@/components/withAuthentication";
 import Navigation from "@/components/Navigation";
 
 const Home: React.FC = () => {
-  const authContext = useContext(AuthContext);
-
-  const handleLogout = () => {
-    authContext.deauthenticate();
-  };
-
   return (
     <div>
       <Navigation />
       <main>
-        <h1>
-          Welcome!
-        </h1>
-        <button onClick={handleLogout}>Log out</button>
+        <h1>Welcome!</h1>
       </main>
     </div>
   );
--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,18 +1,33 @@
+import AuthenticationContext from "@/contexts/AuthContext";
 import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Toolbar from "@mui/material/Toolbar";
+import { useContext } from "react";
 
 export default function Navigation(): React.ReactNode {
+  const authContext = useContext(AuthenticationContext);
+
+  const handleLogout = () => {
+    authContext.deauthenticate();
+  };
+
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Button color="inherit" href="/">
-          Skill Bank
-        </Button>
-        <Button color="inherit" href="/skills">
-          My Skills
-        </Button>
-      </Toolbar>
-    </AppBar>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Button color="inherit" href="/">
+            Skill Bank
+          </Button>
+          <Button color="inherit" href="/skills">
+            My Skills
+          </Button>
+          <Box sx={{ flexGrow: 1 }} />
+          <Button color="inherit" onClick={handleLogout}>
+            Logout
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
