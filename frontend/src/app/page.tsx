b758667bdc848e54d30021fc0701ab69123519ee
Jori Lampi <36279434+jolampi@users.noreply.github.com>
2025-06-05 14:53:57 +0300

    Apply Material UI (#7)

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,28 +1,16 @@
 "use client";
 
-import styles from "./page.module.css";
-import { useContext } from "react";
-import AuthContext from "@/contexts/AuthContext";
-import Link from "next/link";
 import withAuthentication from "@/components/withAuthentication";
+import Navigation from "@/components/Navigation";
 
 const Home: React.FC = () => {
-  const authContext = useContext(AuthContext);
-
-  const handleLogout = () => {
-    authContext.deauthenticate();
-  };
-
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        Welcome!
-        <div className={styles.ctas}>
-          <Link className={styles.primary} href="/skills">
-            Skills
-          </Link>
-        </div>
-        <button onClick={handleLogout}>Log out</button>
+    <div>
+      <header>
+        <Navigation />
+      </header>
+      <main>
+        <h1>Welcome!</h1>
       </main>
     </div>
   );
