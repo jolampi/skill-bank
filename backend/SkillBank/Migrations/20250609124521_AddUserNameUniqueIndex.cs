c479f860ba6be3f8fe6797188630d7fff1d25b7c
Jori Lampi <36279434+jolampi@users.noreply.github.com>
2025-06-09 15:50:15 +0300

    Add unique index on username

---
+++ backend/SkillBank/Migrations/20250609124521_AddUserNameUniqueIndex.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkillBank.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserNameUniqueIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_UserName",
+                table: "AspNetUsers",
+                column: "UserName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_UserName",
+                table: "AspNetUsers");
+        }
+    }
+}
