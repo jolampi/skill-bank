fae4ea4a9b00ad240a0b049598f6bd81a6a95244
Jori Lampi <36279434+jolampi@users.noreply.github.com>
2025-06-13 15:26:28 +0300

    rm orphan param doc

--- backend/SkillBank/Models/Page.cs
+++ backend/SkillBank/Models/Page.cs
@@ -4,7 +4,6 @@ namespace SkillBank.Models;
 /// Wrapper around a full list of results that will be aligned with paged responses.
 /// </summary>
 /// <typeparam name="T">Response item type</typeparam>
-/// <param name="Results">The list.</param>
 public record Unpaged<T>
 {
     public required IEnumerable<T> Results { get; init; }
