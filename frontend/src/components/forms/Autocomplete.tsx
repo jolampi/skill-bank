a5034dd43fea6d989ec3ec17c1abc514f6020812
Jori Lampi <36279434+jolampi@users.noreply.github.com>
2025-07-15 15:31:58 +0300

    Update profile page

---
+++ frontend/src/components/forms/Autocomplete.tsx
@@ -0,0 +1,35 @@
+import MuiAutocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+
+export interface AutocompleteProps {
+  disabled?: boolean;
+  label: string;
+  suggestions: string[];
+  value: string;
+  onChange(newValue: string): void;
+}
+
+export default function Autocomplete(props: AutocompleteProps): React.ReactNode {
+  const { disabled, label, suggestions, value, onChange } = props;
+
+  function handleChange(_event: React.SyntheticEvent, newValue: string) {
+    if (newValue === null) {
+      return;
+    }
+    onChange(newValue);
+  }
+
+  return (
+    <MuiAutocomplete
+      freeSolo
+      selectOnFocus
+      disableClearable
+      disabled={disabled}
+      size="small"
+      options={suggestions}
+      inputValue={value}
+      onInputChange={handleChange}
+      renderInput={(params) => <TextField label={label} {...params} />}
+    />
+  );
+}
