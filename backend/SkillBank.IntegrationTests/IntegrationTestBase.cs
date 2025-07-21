65a83405cc4c3a7f86b9456922f27993dc69ca16
Jori Lampi <36279434+jolampi@users.noreply.github.com>
2025-07-21 18:11:22 +0300

    Add tests

--- backend/SkillBank.IntegrationTests/IntegrationTestBase.cs
+++ backend/SkillBank.IntegrationTests/IntegrationTestBase.cs
@@ -6,17 +6,30 @@ namespace SkillBank.IntegrationTests;
 
 public abstract class IntegrationTestBase : IClassFixture<IntegrationTestApplicationFactory>
 {
+    protected ApplicationDbContext Context { get; }
     protected HttpClient Client { get; }
+
+    private readonly IServiceScope _scope;
 
     public IntegrationTestBase(IntegrationTestApplicationFactory factory)
     {
         Client = factory.CreateClient();
 
-        var scope = factory.Services.CreateScope();
-        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-        if (context.Database.GetPendingMigrations().Any())
+        _scope = factory.Services.CreateScope();
+        Context = _scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        if (Context.Database.GetPendingMigrations().Any())
         {
-            context.Database.Migrate();
+            Context.Database.Migrate();
         }
+        // TODO: Figure out how to disable seeding
+        Context.UserSkills.ExecuteDelete();
+        Context.Skills.ExecuteDelete();
+        Context.Users.ExecuteDelete();
+    }
+
+    public T GetService<T>()
+        where T : notnull
+    {
+        return _scope.ServiceProvider.GetRequiredService<T>();
     }
 }
--- backend/SkillBank/Services/SkillService.cs
+++ backend/SkillBank/Services/SkillService.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using Microsoft.EntityFrameworkCore;
 using SkillBank.Entities;
 using SkillBank.Models;
