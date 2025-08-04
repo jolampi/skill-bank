27a81b69863899a96a808666494ea76e48049fe9
Jori Lampi <36279434+jolampi@users.noreply.github.com>
2025-08-04 15:41:26 +0300

    Tweak

--- frontend/src/app/(authenticated)/consultants/components/SkillChip.tsx
+++ frontend/src/app/(authenticated)/consultants/components/SkillChip.tsx
@@ -1,13 +1,8 @@
-import { SxProps, Theme } from "@mui/material";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
 import Rating from "@/components/forms/Rating";
 import { UserSkill } from "@/types";
-
-const flex: SxProps<Theme> = {
-  display: "flex",
-};
 
 export interface SkillChipProps {
   value: UserSkill;
@@ -17,7 +12,7 @@ export function SkillChip(props: SkillChipProps): React.ReactNode {
   const { value } = props;
 
   return (
-    <Box key={value.label} sx={flex}>
+    <Box key={value.label} sx={{ display: "flex" }}>
       <Typography sx={{ flex: 1, fontSize: 14 }}>{value.label}</Typography>
       <Rating readonly size="small" value={value.proficiency} />
     </Box>
