979e8cb4a4cfdd2ee05914821406fca86d6d6b6d
Jori Lampi <36279434+jolampi@users.noreply.github.com>
2025-08-11 14:13:39 +0300

    Configure for web hosting (#54)

--- frontend/src/generated/client/client.gen.ts
+++ frontend/src/generated/client/client.gen.ts
@@ -7,6 +7,7 @@ import {
   createClient,
   createConfig,
 } from "./client";
+import { createClientConfig } from "../../hey-api";
 
 /**
  * The `createClientConfig()` function will be called on client initialization
@@ -21,7 +22,9 @@ export type CreateClientConfig<T extends DefaultClientOptions = ClientOptions> =
 ) => Config<Required<DefaultClientOptions> & T>;
 
 export const client = createClient(
-  createConfig<ClientOptions>({
-    baseUrl: "http://localhost:5268",
-  }),
+  createClientConfig(
+    createConfig<ClientOptions>({
+      baseUrl: "http://localhost:5268",
+    }),
+  ),
 );
