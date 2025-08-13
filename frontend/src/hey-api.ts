09d579ed3f02bb4c4e3acebae5e53a695474b006
Jori Lampi <36279434+jolampi@users.noreply.github.com>
2025-08-13 17:38:01 +0300

    Use server backend url

--- frontend/src/hey-api.ts
+++ frontend/src/hey-api.ts
@@ -1,7 +1,7 @@
 import { CreateClientConfig } from "./generated/client/client.gen";
 
 export const createClientConfig: CreateClientConfig = (config) => {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  const baseUrl = process.env.BACKEND_URL;
 
   if (!baseUrl) {
     return { ...config };
